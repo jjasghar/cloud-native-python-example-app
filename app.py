ef371bd0d9e2bbd612321591f897e3c03eb74cec
JJ Asghar <jjasghar@gmail.com>
2020-04-24 15:12:08 -0500

    Added a health check
    - It's important to have
    
    Signed-off-by: JJ Asghar <awesome@ibm.com>
    Co-authored-by: Ryan Jarvinen <ryanj@redhat.com>
    Co-authored-by: Brian Tannous <btannous@redhat.com>
    Signed-off-by: JJ Asghar <jjasghar@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import Response
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/healthz")
+def healthz():
+    resp = Response("ok")
+    resp.headers['Custom-Header'] = 'Awesome'
+    return resp
 
 if __name__ == "__main__":
     app.run()
