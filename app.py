3b0590fdb6d41a2e6abd2569d1264752445e4217
JJ Asghar <jjasghar@gmail.com>
2021-01-21 12:07:33 -0600

    cloudday
    Signed-off-by: JJ Asghar <jjasghar@gmail.com>

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello I'm an application!"
+    return "Hello I'm presenting at IBM Cloud day!!!"
  
 
 @app.route("/healthz")
