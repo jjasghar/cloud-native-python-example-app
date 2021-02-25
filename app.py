40e03b5edc6cac11e382885072b72af487bf5f24
JJ Asghar <jja@ibm.com>
2021-02-25 12:23:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello Angelbeat i swear!!!"
+    return "Hello Angelbeat!!!"
  
 
 @app.route("/healthz")
