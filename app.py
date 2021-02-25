ae65d6417536f1ec2ab4c730b70d878155f20ae2
JJ Asghar <awesome@ibm.com>
2021-02-25 09:03:31 -0600

    reset

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello I'm presenting at IBM Cloud day!!!"
+    return "Hello World!!"
  
 
 @app.route("/healthz")
