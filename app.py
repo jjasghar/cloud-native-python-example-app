e73d701efa4a1151af0c960c2fabbf68c0750d6f
JJ Asghar <jja@ibm.com>
2021-03-26 12:28:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello Python Web Conf!"
+    return "I'm happy it's friday!"
  
 
 @app.route("/healthz")
