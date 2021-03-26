d7bc526ea7ac6ea4cd0830c492b678ec7168af19
JJ Asghar <awesome@ibm.com>
2021-03-26 12:22:54 -0500

    python web conf

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello codeengine!"
+    return "Hello Python Web Conf!"
  
 
 @app.route("/healthz")
