582fc3718c47ab5924c0fdb2e0762019d91e62e5
JJ Asghar <awesome@ibm.com>
2021-12-04 14:40:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is an BSOK python app."
+    return "This is the most amazing app EVER."
  
 
 @app.route("/healthz")
