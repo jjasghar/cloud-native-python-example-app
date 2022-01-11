9d068f82efe84b64c5658aed4f318ac590af8647
JJ Asghar <awesome@ibm.com>
2022-01-11 11:12:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is the worse amazing app EVER, speaking at pyjamas."
+    return "This is the worse amazing app EVER, speaking at conf42."
  
 
 @app.route("/healthz")
